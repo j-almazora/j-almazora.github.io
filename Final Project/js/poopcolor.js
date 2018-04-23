a5c4f7a85feae2a84e0ec881505f64c04acdfeed
azterisk <derrizck@gmail.com>
2018-04-23 00:12:31 -0700

    added some pages

---
+++ Final Project/js/poopcolor.js
@@ -0,0 +1,225 @@
+var dot26 = document.getElementById("st26")
+var disp = document.getElementById("display")
+var dot15 =document.getElementById("st15")
+var dot16 =document.getElementById("st16")
+var dot17 =document.getElementById("st17")
+var dot20 =document.getElementById("st20")
+var dot21 =document.getElementById("st21")
+var dot22 =document.getElementById("st22")
+
+var dot18 = document.getElementById("st18")
+var dot28 = document.getElementById("st28")
+var dot34 = document.getElementById("st34")
+var dot35 = document.getElementById("st35")
+var dot36 = document.getElementById("st36")
+
+var dot12 = document.getElementById("st12")
+var dot13 = document.getElementById("st13")
+var dot14 = document.getElementById("st14")
+var dot23 = document.getElementById("st23")
+var dot29 = document.getElementById("st29")
+var dot30 = document.getElementById("st30")
+var dot31 = document.getElementById("st31")
+
+dot26.addEventListener("mousemove",function(){
+    dot26.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "Bloody stool is a sign that there is bleeding somewhere along the digestive tract. "
+})
+dot26.addEventListener("mouseout",function(){
+    dot26.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+//normal//
+dot15.addEventListener("mousemove",function(){
+    dot15.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "Congrats! Your poop is healthy! When overnutrition, the color will get darker. Conversely, the color will become lighter."
+})
+dot15.addEventListener("mouseout",function(){
+    dot15.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+
+dot16.addEventListener("mousemove",function(){
+    dot16.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "Congrats! Your poop is healthy! When overnutrition, the color will get darker. Otherwise, the color will become lighter."
+})
+dot16.addEventListener("mouseout",function(){
+    dot16.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot17.addEventListener("mousemove",function(){
+    dot17.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "Congrats! Your poop is healthy! When overnutrition, the color will get darker. Conversely, the color will become lighter."
+})
+dot17.addEventListener("mouseout",function(){
+    dot17.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot20.addEventListener("mousemove",function(){
+    dot20.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "Congrats! Your poop is healthy! When overnutrition, the color will get darker. Conversely, the color will become lighter."
+})
+dot20.addEventListener("mouseout",function(){
+    dot20.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot21.addEventListener("mousemove",function(){
+    dot21.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "Congrats! Your poop is healthy! When overnutrition, the color will get darker. Conversely, the color will become lighter."
+})
+dot21.addEventListener("mouseout",function(){
+    dot21.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot22.addEventListener("mousemove",function(){
+    dot22.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "Congrats! Your poop is healthy! When overnutrition, the color will get darker. Conversely, the color will become lighter."
+})
+dot22.addEventListener("mouseout",function(){
+    dot22.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+
+//end normal//
+
+//constipation//
+dot18.addEventListener("mousemove",function(){
+    dot18.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's constipation. Color gets darker if poop stays in large intestine for too long. Even waste has been absorbed. Skin condition is getting worse. Pay attention."
+})
+dot18.addEventListener("mouseout",function(){
+    dot18.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+
+dot28.addEventListener("mousemove",function(){
+    dot28.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's constipation. Color gets darker if poop stays in large intestine for too long. Even waste has been absorbed. Skin condition is getting worse. Pay attention."
+})
+dot28.addEventListener("mouseout",function(){
+    dot28.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+
+dot34.addEventListener("mousemove",function(){
+    dot34.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's constipation. Color gets darker if poop stays in large intestine for too long. Even waste has been absorbed. Skin condition is getting worse. Pay attention."
+})
+dot34.addEventListener("mouseout",function(){
+    dot34.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+
+dot35.addEventListener("mousemove",function(){
+    dot35.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's constipation. Color gets darker if poop stays in large intestine for too long. Even waste has been absorbed. Skin condition is getting worse. Pay attention."
+})
+dot35.addEventListener("mouseout",function(){
+    dot35.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+
+dot36.addEventListener("mousemove",function(){
+    dot36.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's constipation. Color gets darker if poop stays in large intestine for too long. Even waste has been absorbed. Pay attention."
+})
+dot36.addEventListener("mouseout",function(){
+    dot36.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+
+//inanition//
+dot12.addEventListener("mousemove",function(){
+    dot12.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's a sign of lacking nourishment. It's unusual to see. As long as eating properly, the color will be healthy brown."
+})
+dot12.addEventListener("mouseout",function(){
+    dot12.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot13.addEventListener("mousemove",function(){
+    dot13.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's a sign of lacking nourishment. It's unusual to see. As long as eating properly, the color will be healthy brown."
+})
+dot13.addEventListener("mouseout",function(){
+    dot13.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot14.addEventListener("mousemove",function(){
+    dot14.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's a sign of lacking nourishment. It's unusual to see. As long as eating properly, the color will be healthy brown."
+})
+dot14.addEventListener("mouseout",function(){
+    dot14.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot23.addEventListener("mousemove",function(){
+    dot23.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's a sign of lacking nourishment. It's unusual to see. As long as eating properly, the color will be healthy brown."
+})
+dot23.addEventListener("mouseout",function(){
+    dot23.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot29.addEventListener("mousemove",function(){
+    dot29.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's a sign of lacking nourishment. It's unusual to see. As long as eating properly, the color will be healthy brown."
+})
+dot29.addEventListener("mouseout",function(){
+    dot29.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot30.addEventListener("mousemove",function(){
+    dot30.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's a sign of lacking nourishment. It's unusual to see. As long as eating properly, the color will be healthy brown."
+})
+dot30.addEventListener("mouseout",function(){
+    dot30.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
+dot31.addEventListener("mousemove",function(){
+    dot31.style.r = "40"
+    disp.style.display = "inline-block"
+    disp.innerHTML = "It's a sign of lacking nourishment. It's unusual to see. As long as eating properly, the color will be healthy brown."
+})
+dot31.addEventListener("mouseout",function(){
+    dot31.style.r = "33.5"
+    disp.style.display = "none"
+    
+})
